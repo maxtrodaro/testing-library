2da39b659ab908fce186d1ed52eed9bfb2da83df
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2021-03-26 02:26:23 -0300

    Second validate error test

--- src/useKonamiCode.js
+++ src/useKonamiCode.js
@@ -1,15 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+export const konamiSequence = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a",
+];
 
 export default function useKonamiCode() {
-  let sequence = [];
+  const [sequence, setSequence] = useState([]);
 
-  const onKeyDown = (event) => sequence.push(event.key);
+  const onKeyDown = (event) => setSequence((prev) => [...prev, event.key]);
+
+  useEffect(() => {
+    sequence.forEach((key, i) => {
+      if (key !== konamiSequence[i]) {
+        setSequence([]);
+      }
+    });
+  }, [sequence]);
 
   useEffect(() => {
     window.addEventListener("keydown", onKeyDown);
 
     return () => window.removeEventListener("keydown", onKeyDown);
-  });
+  }, []);
 
   return { sequence };
 }
--- src/useKonamiCode.test.js
+++ src/useKonamiCode.test.js
@@ -12,4 +12,15 @@ describe("useKonamiCode", () => {
 
     expect(result.current.sequence).toEqual(["ArrowUp"]);
   });
+
+  it("should reset the sequence if the user types a wrong sequence", () => {
+    const { result } = renderHook(() => useKonamiCode());
+
+    act(() => {
+      window.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrowUp" }));
+      window.dispatchEvent(new KeyboardEvent("keydown", { key: "k" }));
+    });
+
+    expect(result.current.sequence).toEqual([]);
+  });
 });
